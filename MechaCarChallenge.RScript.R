b1f97a19135b03b808ca93503622edde72d4c5cb
Karganleh  Borh <karganlehborh@Karganlehs-MacBook-Pro.local>
2021-04-11 16:02:25 -0500

    initial commit

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,20 @@
+#load dplyr package
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+library(jsonlite)
+
+#read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
+
+
+
+
+
