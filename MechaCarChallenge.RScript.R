54bf7a7533c7b1653928ea11fb8f427997cb69ad
Karganleh  Borh <karganlehborh@Karganlehs-MacBook-Pro.local>
2021-04-15 21:15:01 -0500

    final commit

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -14,6 +14,30 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
 
 
+#read file
+Suspension <- read.csv(file='Suspension_coil.csv',check.names=F,stringsAsFactors = F)
+
+#get a total summary
+total_summery <- Suspension %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) 
+
+#create a lot summary
+lot_summery <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI),.groups='keep')
+
+#perform t-test to determine if the PSI across 
+# Peform t-test across all Lots
+t.test(Suspension$PSI,mu = 1500)
+
+# Peform t-test on Lot 1
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+
+# Peform t-test on Lot 2
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+
+# Peform t-test on Lot 3
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
+
+
 
 
 
